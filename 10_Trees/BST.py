868de4cc746997db2fa68c752bac03b0a16f0a07
Irina <golubtsovairinas@gmail.com>
2021-10-08 18:43:13 -0700

    tree bst remove func

--- 10_Trees/BST.py
+++ 10_Trees/BST.py
@@ -21,7 +21,7 @@ class Node:
 
     def search(self, search_value):
         if self.value == search_value:
-            return self.value
+            return self
         elif self.value > search_value:
             if self.left is None:
                 return f'{search_value} is not in a Tree'
@@ -31,13 +31,19 @@ class Node:
                 return f'{search_value} is not in a Tree'
             return self.right.search(search_value)
 
-    def remove(self, root, remove_value):
-
+    def remove(self, remove_value):
+        if self is None:
+            return self
         if remove_value == self.value:
+            if not self.left and not self.right:
+                self = None
+                return self
+
             if not self.left:
                 temp = self.right
                 self.right = None
                 return temp
+
             elif not self.right:
                 temp = self.left
                 self.left = None
@@ -46,17 +52,19 @@ class Node:
             # the biggest in the left subtree
             last_right_in_left = self.left.find_max_in_left()
             self.value = last_right_in_left.value
-            return self.remove(last_right_in_left, self.value)
+            last_right_in_left.remove(self.value)
 
 
         elif remove_value < self.value:
             if self.left is None:
-                return f'{remove_value} is not in a Tree'
-            self.left = self.left.remove(root, remove_value)
+                print( f'{remove_value} is not in a Tree')
+            else:
+                self.left = self.left.remove(remove_value)
         else:
             if self.right is None:
-                return f'{remove_value} is not in a Tree'
-            self.right = self.right.remove(root, remove_value)
+                print( f'{remove_value} is not in a Tree')
+            else:
+                self.right = self.right.remove(remove_value)
 
         return self
 
@@ -90,14 +98,16 @@ def check_if_bst(root):
 if __name__ == "__main__":
     root = Node(59)
     # number_list = list(set(round(random.random() * 100) for _ in range(20)))
-    number_list = [45,75,40,51,71,77,35,42,47,53,41,43,46,49,52,55]
+    number_list = [45, 75, 40, 51, 71, 77, 35, 42, 47, 53, 41, 43, 46, 49, 52,
+                   55]
     for number in number_list:
         root.insert(number)
     print(check_if_bst(root))
     # print(root.search(8))
     print(root.search(16))
-    print(root.remove(root, 37))
+    root.remove(55)
+    root.remove(51)
     # print(root.remove(root, 16))
-    root.remove(root, 10)
-
+    root.remove(10)
+    print()
     root.print_tree()
